09d219e911f0e1b594ca62fb4ae990583b633ed2
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-02-19 17:48:55 +0100

    recreated everything using express framework

--- index.js
+++ index.js
@@ -1,28 +1,87 @@
-const http = require("node:http");
-const fs = require("node:fs");
-let fileName;
+// const http = require("node:http");
+// const fs = require("node:fs");
+// let fileName;
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        fileName = "./index.html";
-    } else if (req.url === "/about") {
-        fileName = "./about.html";
-    } else if (req.url === "/contact-me") {
-        fileName = "./contact-me.html";
-    } else {
-        fileName = "./404.html";
+// const server = http.createServer((req, res) => {
+//     if (req.url === "/") {
+//         fileName = "./index.html";
+//     } else if (req.url === "/about") {
+//         fileName = "./about.html";
+//     } else if (req.url === "/contact-me") {
+//         fileName = "./contact-me.html";
+//     } else {
+//         fileName = "./404.html";
+//     }
+//     fs.readFile(fileName, (err, data) => {
+//         if (err) {
+//             res.writeHead(500, { "Content-type": "text-html" });
+//             res.write(data);
+//             res.end();
+//         } else {
+//             res.writeHead(200, { "Content-type": "text/html" });
+//             res.write(data);
+//             res.end();
+//         }
+//     })
+// })
+
+// server.listen(8080);
+
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.get("/", (req, res) => {
+    options = {
+        root: path.join(__dirname)
     }
-    fs.readFile(fileName, (err, data) => {
+    res.sendFile("index.html", options, (err) => {
         if (err) {
-            res.writeHead(500, { "Content-type": "text-html" });
-            res.write(data);
-            res.end();
+            console.log(err);
         } else {
-            res.writeHead(200, { "Content-type": "text/html" });
-            res.write(data);
-            res.end();
+            console.log("Success");
         }
-    })
+    });
+})
+app.get("/about", (req, res) => {
+    options = {
+        root: path.join(__dirname)
+    }
+    res.sendFile("about.html", options, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Success");
+        }
+    });
 })
 
-server.listen(8080);
+app.get("/contact-me", (req, res) => {
+    options = {
+        root: path.join(__dirname)
+    }
+    res.sendFile("contact-me.html", options, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Success");
+        }
+    });
+})
+
+
+app.get("*", (req, res) => {
+    options = {
+        root: path.join(__dirname)
+    }
+    res.sendFile("404.html", options, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Success");
+        }
+    } )
+})
+
+app.listen(5555);
+
